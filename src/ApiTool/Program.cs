c890fefbe07140b8dd914bd97b8b57e723714714
Brian Cristante <brcrista@microsoft.com>
2019-07-20 14:24:52 -0400

    We might not need to get each pull request

--- src/ApiTool/Program.cs
+++ src/ApiTool/Program.cs
@@ -48,22 +48,6 @@ namespace ApiTool
             await outputFileStore.WriteFileAsync(
                 filename: "pull-requests.json",
                 content: JsonConvert.SerializeObject(pullRequests, Formatting.Indented));
-
-            var pullRequestIds = from pr in pullRequests
-                where !outputFileStore.Contains(pr.PullRequestId.ToString())
-                select pr.PullRequestId;
-
-            foreach (var pullRequestId in pullRequestIds)
-            {
-                var pullRequest = await restClient.GetPullRequestAsync(
-                    project: project,
-                    repository: repository,
-                    pullRequestId: pullRequestId);
-
-                await outputFileStore.WriteFileAsync(
-                    filename: $"pull-requests/{pullRequestId.ToString()}.json",
-                    content: JsonConvert.SerializeObject(pullRequest, Formatting.Indented));
-            }
         }
     }
 }
