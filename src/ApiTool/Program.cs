befb742fc0ffb0e469ac7b16eba5e89592c4eb11
Brian Cristante <brcrista@microsoft.com>
2019-07-20 14:21:04 -0400

    Factor out main logic into a separate method

--- src/ApiTool/Program.cs
+++ src/ApiTool/Program.cs
@@ -13,7 +13,7 @@ namespace ApiTool
             var parserResult = Parser.Default.ParseArguments<CommandLineOptions>(args);
 
             await parserResult.MapResult(
-                async options =>
+                options =>
                 {
                     var restClient = new RestClient(
                         organizationUri: new Uri(options.Url, UriKind.Absolute),
@@ -22,35 +22,48 @@ namespace ApiTool
                     var outputDirectory = options.OutputDirectory ?? CommandLineOptions.DefaultOutputDirectory;
                     var outputFileStore = new DataFileStore(outputDirectory);
 
-                    var pullRequests = await restClient.GetPullRequestsAsync(
+                    return FetchDataFilesAsync(
+                        restClient,
+                        outputFileStore,
                         project: options.Project,
                         repository: options.Repository);
-
-                    await outputFileStore.WriteFileAsync(
-                        filename: "pull-requests/list.json",
-                        content: JsonConvert.SerializeObject(pullRequests, Formatting.Indented));
-
-                    var pullRequestIds = from pr in pullRequests
-                        where !outputFileStore.Contains(pr.PullRequestId.ToString())
-                        select pr.PullRequestId;
-
-                    foreach (var pullRequestId in pullRequestIds)
-                    {
-                        var pullRequest = await restClient.GetPullRequestAsync(
-                            project: options.Project,
-                            repository: options.Repository,
-                            pullRequestId: pullRequestId);
-
-                        await outputFileStore.WriteFileAsync(
-                            filename: $"pull-requests/{pullRequestId.ToString()}.json",
-                            content: JsonConvert.SerializeObject(pullRequest, Formatting.Indented));
-                    }
                 },
                 errors =>
                 {
                     // ParseArguments will already print an error message when failing to parse
                     return Task.CompletedTask;
                 });
+        }
+
+        private static async Task FetchDataFilesAsync(
+            RestClient restClient,
+            DataFileStore outputFileStore,
+            string project,
+            string repository)
+        {
+            var pullRequests = await restClient.GetPullRequestsAsync(
+                project: project,
+                repository: repository);
+
+            await outputFileStore.WriteFileAsync(
+                filename: "pull-requests.json",
+                content: JsonConvert.SerializeObject(pullRequests, Formatting.Indented));
+
+            var pullRequestIds = from pr in pullRequests
+                where !outputFileStore.Contains(pr.PullRequestId.ToString())
+                select pr.PullRequestId;
+
+            foreach (var pullRequestId in pullRequestIds)
+            {
+                var pullRequest = await restClient.GetPullRequestAsync(
+                    project: project,
+                    repository: repository,
+                    pullRequestId: pullRequestId);
+
+                await outputFileStore.WriteFileAsync(
+                    filename: $"pull-requests/{pullRequestId.ToString()}.json",
+                    content: JsonConvert.SerializeObject(pullRequest, Formatting.Indented));
+            }
         }
     }
 }
